5c46cff059402acec91f7244b3a4c8b294cc2d72
Devesh Avutu <devesh14may@gmail.com>
2020-05-05 19:34:41 +0530

    setting up bottom navigation

--- App.js
+++ App.js
@@ -1,2 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+	MaterialCommunityIcons,
+	Feather,
+	SimpleLineIcons,
+	Ionicons,
+} from "@expo/vector-icons";
+import HomeScreen from "./src/screens/HomeScreen";
+import FavouritesScreen from "./src/screens/FavouritesScreen";
+import OrdersScreen from "./src/screens/OrdersScreen";
+import BasketScreen from "./src/screens/BasketScreen";
+import SearchScreen from "./src/screens/SearchScreen";
+
+const Tab = createBottomTabNavigator();
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen
+					name="Home"
+					component={HomeScreen}
+					options={{
+						tabBarIcon: () => (
+							<MaterialCommunityIcons name="home-outline" size={25} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Favourites"
+					component={FavouritesScreen}
+					options={{
+						tabBarIcon: () => (
+							<MaterialCommunityIcons name="heart-outline" size={25} />
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Search"
+					component={SearchScreen}
+					options={{
+						tabBarIcon: () => <Ionicons name="md-search" size={28} />,
+					}}
+				/>
+				<Tab.Screen
+					name="Orders"
+					component={OrdersScreen}
+					options={{
+						tabBarIcon: () => <Feather name="briefcase" size={25} />,
+					}}
+				/>
+				<Tab.Screen
+					name="Basket"
+					component={BasketScreen}
+					options={{
+						tabBarIcon: () => <SimpleLineIcons name="basket" size={25} />,
+					}}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+};
+export default App;
---
+++ src/screens/BasketScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const BasketScreen = () => {
+	return (
+		<View>
+			<Text>BasketScreen</Text>
+		</View>
+	);
+};
+
+export default BasketScreen;
