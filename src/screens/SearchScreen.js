4cf880382508832dd042cd6aa8e3f0b80c8cb3c8
Devesh Avutu <devesh14may@gmail.com>
2020-05-06 16:00:32 +0530

    styling the search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import commonStyles from "../styles";
 
 const SearchScreen = () => {
 	return (
-		<View>
-			<Text>SearchScreen</Text>
+		<View style={{ flex: 1 }}>
+			<TextInput
+				style={styles.searchTextInput}
+				placeholder="Search for a product"
+			/>
+			<View style={styles.searchEmptyContainer}>
+				<Text style={commonStyles.Text_H5}>Search Groceries</Text>
+				<Text style={commonStyles.Text_T2}>What are you looking for?</Text>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	searchTextInput: {
+		height: 60,
+		backgroundColor: "white",
+		padding: 20,
+	},
+	searchEmptyContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
 
 export default SearchScreen;
