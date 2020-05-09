508b6995ef4b6b8f1c80c1d0668836bde84a54bd
Devesh Avutu <devesh14may@gmail.com>
2020-05-09 22:52:34 +0530

    created icons for items in basket

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, Image, View, StyleSheet } from "react-native";
 import Button from "../components/Button";
 import commonStyles from "../styles";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default ({ product }) => {
 	return (
@@ -12,28 +13,46 @@ export default ({ product }) => {
 				}}
 				style={styles.imageStyle}
 			/>
-			<View style={{ marginHorizontal: 10 }}>
-				<Text style={styles.productTitle}>{product.title}</Text>
+			<View style={{ marginHorizontal: 10, flex: 1 }}>
+				<Text
+					style={styles.productTitle}
+					numberOfLines={2}
+					ellipsizeMode="tail"
+				>
+					{product.title}
+				</Text>
 				<View style={styles.priceViewStyle}>
-					<View style={{ marginRight: 40 }}>
+					<View>
 						<Text style={commonStyles.Text_H4}>£{product.price.actual}</Text>
 						<Text
 							style={commonStyles.Text_T4}
 						>{`£${product.price.actual}/${product.price.unitOfMeasure}`}</Text>
 					</View>
-					<Button
+					{/* <Button
 						text="Add"
 						textStyle={commonStyles.PrimaryButtonText}
 						buttonStyle={commonStyles.PrimaryButtonSmall}
-					/>
+					/> */}
+					{renderButtons()}
 				</View>
 			</View>
 		</View>
 	);
 };
 
+const renderButtons = () => {
+	return (
+		<View style={styles.buttonsContainer}>
+			<MaterialIcons name="add-circle" size={40} color="#00539F" />
+			<Text style={styles.quantityText}>1</Text>
+			<MaterialIcons name="remove-circle" size={40} color="#00539F" />
+		</View>
+	);
+};
+
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		flexDirection: "row",
 		backgroundColor: "white",
 		paddingVertical: 10,
@@ -55,11 +74,22 @@ const styles = StyleSheet.create({
 		marginHorizontal: 15,
 	},
 	priceViewStyle: {
+		flex: 1,
 		flexDirection: "row",
 		marginTop: 10,
 	},
 	productTitle: {
 		...commonStyles.Text_H5,
 		color: "#00539F",
+	},
+	buttonsContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "flex-end",
+	},
+	quantityText: {
+		...commonStyles.Text_H3,
+		marginHorizontal: 10,
+		marginTop: 6,
 	},
 });
